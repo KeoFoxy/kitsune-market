ada8bc7173dadf18973234839ab4e292479dffb4
KeoFoxy <arasdiel68@gmail.com>
2023-02-13 23:11:53 +0300

    New button as component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { HTag } from "../../components";
+import { Button, HTag } from "../../components";
 
 export default function Home() {
   return (
     <>
       <HTag tag='h1'>Text</HTag>
+      <Button appearance="primary" className="Super">Button</Button>
+      <Button appearance="ghost">Ghost</Button>
     </>
   )
 }
