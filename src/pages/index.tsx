d376e84f5eaa9ea5549428aaa94ecce5b8f9c3d0
KeoFoxy <arasdiel68@gmail.com>
2023-03-12 17:31:02 +0300

    Rating component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Button, HTag, Tag, P } from "../../components";
+import { useState, useEffect } from "react";
+import { Button, HTag, Tag, P, Rating } from "../../components";
 
 export default function Home() {
+
+  const [rating, setRating] = useState<number>(4);
   return (
     <>
-      <HTag tag='h1'>Text</HTag>
-      <Button appearance="primary" arrow="right">Button</Button>
+      <HTag tag='h1'>Title</HTag>
+      <Button appearance="primary" arrow="right" >Button</Button>
       <Button appearance="ghost" arrow="down">Ghost</Button>
       <P size="l">Loren Ipsum</P>
       <P>Loren Ipsum Medium</P>
@@ -14,6 +17,7 @@ export default function Home() {
       <Tag color="gray"> Grey</Tag>
       <Tag color="primary">Primary</Tag>
       <Tag color="ghost">Ghost</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
     </>
   )
 }
